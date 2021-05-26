87b92d7153d9efd9e3d7478104134adbc9151e19
sevval <ssevvalsenel@gmail.com>
2021-05-26 23:24:52 +0300

    yaptiklarim

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,25 +11,94 @@ import business.concretes.NeroCustomerManager;
 import business.concretes.StarbucksCustomerManager;
 import entities.abstracts.Entity;
 import entities.concretes.Customer;
+import java.util.Scanner;
 
 
 
 public class Main {
+	
+	;
+	
+	
+	public static void istek1() {
+		String istek1;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Yapmak istediðiniz iþlem aþaðýdakilerden hangisidir?Aþaðýya yazýnýz.");
+		System.out.println("1) Müþteri ekleme");
+		System.out.println("2) Müþteri sýralama");
+		istek1= sc.next();
+		System.out.println();
+		
+		switch(istek1) {
+		
+		case "1":
+			musteriekle1();
+			break;
+		case "2":
+			musterisirala1();
+			break;
+	
+		}
+	}
+	
+	
+	public static void musterisirala1() {
+		String musterisirala1;
+		Customer musteri = new Customer(
+				0, firstname() , lastname(), dateOfBirth() , nationalityId()
+				) ; 
+	}
+	
+	public static void musteriekle1() {
+		String musteriekle1;
+		Customer musteri = new Customer(
+				0, firstname() , lastname(), dateOfBirth() , nationalityId()
+				) ; 
+	}
+	
+	public static String firstname() {
+		String firstname;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Lütfen müþteri adýný giriniz = ");
+		firstname=sc.next();
+		System.out.println("Müþteri adý " + firstname);
+		return firstname ;
+	}
+	
+	public static String lastname() {
+		String lastname;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Lütfen müþteri soyadýný giriniz = ");
+		lastname=sc.next();
+		System.out.println("Müþteri soyadý " + lastname);
+		return lastname;
+	}
+	
+	public static LocalDate dateOfBirth() {
+		int yil;
+		int ay;
+		int gun;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Lütfen müþteri doðum gününü yýl, ay, gün olarak giriniz. = ");
+		yil= sc.nextInt();
+		ay= sc.nextInt();
+		gun= sc.nextInt();
+		return LocalDate.of(yil,ay,gun);
+	}
+	
+	public static String nationalityId() {
+		String nationalityId;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Lütfen müþteri ülke numarasýný giriniz = ");
+		nationalityId=sc.next();
+		System.out.println("Müþteri ülke numarasý: " + nationalityId);
+		return nationalityId; 
+	}
+	
 
 	public static void main(String[] args) {
 		
-		
-		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MerniceServiceAdapter());
-		
-		customerManager.Save(
-				new Customer (
-						1,
-						"Memati",
-						"Baþ",
-						LocalDate.of(1982, 12, 01),
-						"12345678901"
-					)
-				);
+		istek1();
 		
 		
 	}
